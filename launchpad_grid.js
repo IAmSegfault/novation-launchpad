ffc9146a055843c023aae1dff49178370d8a29da
educk <edwardcufaude@googlemail.com>
2014-10-07 18:26:54 +0100

    Update launchpad_grid.js

--- launchpad_grid.js
+++ launchpad_grid.js
@@ -13,6 +13,7 @@ gridPage.canScrollScenesUp = false;
 gridPage.canScrollScenesDown = false;
 gridPage.title = "Clip Launcher";
 
+// Updates the scroll buttons
 gridPage.updateOutputState = function()
 {
    clear();
@@ -25,6 +26,7 @@ gridPage.updateOutputState = function()
    this.updateScrollButtons();
    this.updateGrid();
 
+   // Set the top LED while in Clip Launcher
    setTopLED(4,
       TEMPMODE == TempMode.SCENE
          ? Colour.GREEN_FULL
@@ -37,16 +39,24 @@ gridPage.updateOutputState = function()
          ? Colour.GREEN_FULL
          : Colour.OFF);*/
 
+	// Set the top LED of Mixer red when mixer alligned
    setTopLED(7, this.mixerAlignedGrid ? Colour.RED_FULL : Colour.RED_LOW);
 };
 
+// This detects when the Mixer button is pressed and changes the orientation identifier mixerAlignedGrid and displays the text popup
 gridPage.onShift = function(isPressed)
 {
    if (isPressed)
    {
       this.mixerAlignedGrid = !this.mixerAlignedGrid;
+	  if(this.mixerAlignedGrid){
+		application.setPerspective('MIX');
+		}
+	  if(this.mixerAlignedGrid == false){
+	  	application.setPerspective('ARRANGE');
+		}
       //this.setTempMode(TempMode.TRACK);
-      host.showPopupNotification("Orientation: " + (this.mixerAlignedGrid ? "Mixer" : "Arranger"));
+      host.showPopupNotification("Orientation: " + (this.mixerAlignedGrid ? "Mix" : "Arranger"));
    }
    else
    {
@@ -54,6 +64,7 @@ gridPage.onShift = function(isPressed)
    }
 }
 
+// This detects when one of the vertical buttons is pressed and changes the TempMode
 gridPage.onSceneButton = function(row, isPressed)
 {
    if (isPressed)
@@ -99,6 +110,7 @@ gridPage.onSceneButton = function(row, isPressed)
    }
 };
 
+// These following 4 functions control the scrolling arrow buttons allowing move around
 gridPage.onLeft = function(isPressed)
 {
    if (isPressed)
@@ -131,7 +143,9 @@ gridPage.onDown = function(isPressed)
    if (isPressed)
    {
       if (this.mixerAlignedGrid) trackBank.scrollScenesDown();
-      else trackBank.scrollTracksDown();
+      else{
+		trackBank.scrollTracksDown();
+	  }
    }
 };
 
@@ -163,68 +177,107 @@ gridPage.onGridButton = function(row, column, pressed)
    }
    else if (TEMPMODE === TempMode.TRACK)
    {
-      switch(column)
-      {
-         case TrackModeColumn.STOP:
-            trackBank.getTrack(row).getClipLauncher().stop();
-            break;
+	
+	  var track = this.mixerAlignedGrid ? column : row;
+      var scene = this.mixerAlignedGrid ? row-2 : column;
+	
+	 if (this.mixerAlignedGrid == false) {
+      switch(scene)
+		  {
+			 case TrackModeColumn.STOP:
+				trackBank.getTrack(track).getClipLauncher().stop();
+				break;
 
-         case TrackModeColumn.SELECT:
-            trackBank.getTrack(row).select();
-            break;
+			 case TrackModeColumn.SELECT:
+				trackBank.getTrack(track).select();
+				break;
 
-         case TrackModeColumn.MUTE:
-            trackBank.getTrack(row).getMute().toggle();
-            break;
+			 case TrackModeColumn.ARM:
+				trackBank.getTrack(track).getArm().toggle();
+				break;
+				
+			 case TrackModeColumn.SOLO:
+				trackBank.getTrack(track).getSolo().toggle();
+				break;
+				
+			 case TrackModeColumn.MUTE:
+				trackBank.getTrack(track).getMute().toggle();
+				break;
 
-         case TrackModeColumn.SOLO:
-            trackBank.getTrack(row).getSolo().toggle();
-            break;
+			 case TrackModeColumn.RETURN_TO_ARRANGEMENT:
+				trackBank.getTrack(track).getClipLauncher().returnToArrangement();
+				break;
+		  }
+	 }
+	 else {
+		  switch(scene)
+		  {
+		  	 case TrackModeColumn.RETURN_TO_ARRANGEMENT-9:
+				trackBank.getTrack(track).getClipLauncher().returnToArrangement();
+				break;
+				
+			 case TrackModeColumn.SELECT:
+				trackBank.getTrack(track).select();
+				break;
 
-         case TrackModeColumn.ARM:
-            trackBank.getTrack(row).getArm().toggle();
-            break;
+			 case TrackModeColumn.ARM:
+				trackBank.getTrack(track).getArm().toggle();
+				break;
+				
+			 case TrackModeColumn.SOLO:
+				trackBank.getTrack(track).getSolo().toggle();
+				break;
+				
+			 case TrackModeColumn.MUTE:
+				trackBank.getTrack(track).getMute().toggle();
+				break;
+			
+			 case TrackModeColumn.STOP+5:
+				trackBank.getTrack(track).getClipLauncher().stop();
+				break;
 
-         case TrackModeColumn.RETURN_TO_ARRANGEMENT:
-            trackBank.getTrack(row).getClipLauncher().returnToArrangement();
-            break;
-      }
+		  }
+	 }	  
+	  
+
    }
+   // setups the buttons functionality (not the colors) for all the Vol, Pan, Sends and user pages.
    else
    {
       switch(TEMPMODE)
       {
          case TempMode.VOLUME:
-            trackBank.getTrack(row).getVolume().set(column, 8);
+            trackBank.getTrack(this.mixerAlignedGrid ? column : row).getVolume().set(this.mixerAlignedGrid ? -Math.abs(row)+7 : column, 8);
             break;
 
          case TempMode.PAN:
-            trackBank.getTrack(row).getPan().set(column, 8);
+            trackBank.getTrack(this.mixerAlignedGrid ? column : row).getPan().set(this.mixerAlignedGrid ? -Math.abs(row)+7 : column, userVarPans);
             break;
 
          case TempMode.SEND_A:
-            trackBank.getTrack(row).getSend(0).set(column, 8);
+            trackBank.getTrack(this.mixerAlignedGrid ? column : row).getSend(0).set(this.mixerAlignedGrid ? -Math.abs(row)+7 : column, 8);
             break;
 
          case TempMode.SEND_B:
-            trackBank.getTrack(row).getSend(1).set(column, 8);
+            trackBank.getTrack(this.mixerAlignedGrid ? column : row).getSend(1).set(this.mixerAlignedGrid ? -Math.abs(row)+7 : column, 8);
             break;
 
          case TempMode.USER1:
-            userControls.getControl(row).set(column, 8);
+            userControls.getControl(this.mixerAlignedGrid ? column : row).set(this.mixerAlignedGrid ? -Math.abs(row)+7 : column, 8);
             break;
 
          case TempMode.USER2:
-            userControls.getControl(row + 8).set(column, 8);
+            userControls.getControl(this.mixerAlignedGrid ? column : row + 8).set(this.mixerAlignedGrid ? -Math.abs(row + 8)+7 : column, 8);
             break;
 
          case TempMode.USER3:
-            userControls.getControl(row + 16).set(column, 8);
+            userControls.getControl(this.mixerAlignedGrid ? column : row + 16).set(this.mixerAlignedGrid ? -Math.abs(row + 16)+7 : column, 8);
             break;
       }
    }
 };
 
+// updates the grid and VUmeters
 gridPage.updateGrid = function()
 {
    for(var t=0; t<8; t++)
@@ -234,6 +287,8 @@ gridPage.updateGrid = function()
    }
 };
 
+// sets the colours for the VUmeters
+// calls the mixColour function within the launchpad_constants.js script
 function vuLevelColor(level)
 {
    switch (level)
@@ -263,6 +318,7 @@ function vuLevelColor(level)
    return Colour.OFF;
 }
 
+// even though this section is called updateVumeter, it also setups the colours of all side buttons when they are pressed
 gridPage.updateVuMeter = function(track)
 {
    var val = vuMeter[track];
@@ -277,7 +333,7 @@ gridPage.updateVuMeter = function(track)
    {
       colour = vuLevelColor(val);
    }
-
+	// Sets the colours of all the right hand side buttons when they are pressed.
    switch(TEMPMODE)
    {
       case TempMode.VOLUME:
@@ -289,15 +345,15 @@ gridPage.updateVuMeter = function(track)
          break;
 
       case TempMode.SEND_A:
-         if (track === 2) colour = Colour.GREEN_FULL;
+         if (track === 2) colour = Colour.YELLOW_FULL;
          break;
 
       case TempMode.SEND_B:
-         if (track === 3) colour = Colour.GREEN_FULL;
+         if (track === 3) colour = Colour.YELLOW_FULL;
          break;
 
       case TempMode.USER1:
-         if (track === 4) colour = Colour.AMBER_FULL;
+         if (track === 4) colour = Colour.GREEN_FULL;
          break;
 
       case TempMode.USER2:
@@ -309,17 +365,19 @@ gridPage.updateVuMeter = function(track)
          break;
 
       case TempMode.TRACK:
-         if (track === 7) colour = Colour.YELLOW_FULL;
+         if (track === 7) colour = Colour.ORANGE;
          break;
    }
 
    setRightLED(track, colour);
 };
 
+
+
 gridPage.updateTrackValue = function(track)
 {
    if (activePage != gridPage) return;
-
+	// this section draws the pads for the main clip launcher
    if (TEMPMODE == TempMode.OFF || TEMPMODE == TempMode.SCENE)
    {
       for(var scene=0; scene<8; scene++)
@@ -327,9 +385,9 @@ gridPage.updateTrackValue = function(track)
          var i = track + scene*8;
 
          var col = arm[track] ? Colour.RED_LOW : Colour.OFF;
-
+		 
          var fullval = mute[track] ? 1 : 3;
-
+		 
          if (hasContent[i] > 0)
          {
             if (isQueued[i] > 0)
@@ -353,30 +411,57 @@ gridPage.updateTrackValue = function(track)
          setCellLED(this.mixerAlignedGrid ? track : scene, this.mixerAlignedGrid ? scene : track, col);
       }
    }
+	// this sets the buttons and lights for the solo/mute/arm track page. The variable TrackModeColumn is set in the main Launchpad script, so reordering them doesn't work.
    else if (TEMPMODE == TempMode.TRACK)
    {
-      for(var scene=5; scene<8; scene++)
-      {
-         setCellLED(scene, track, Colour.OFF);
-      }
+	 if (this.mixerAlignedGrid == false) {
+		  for(var scene=5; scene<8; scene++)
+		  {
+			 setCellLED(scene, track, Colour.OFF);
+		  }
 
-      if (trackExists[track])
-      {
-         setCellLED(TrackModeColumn.SELECT, track, isSelected[track] ?  Colour.AMBER_FLASHING : Colour.AMBER_LOW);
-         setCellLED(TrackModeColumn.MUTE, track, mute[track] ? Colour.GREEN_LOW : Colour.GREEN_FULL);
-         setCellLED(TrackModeColumn.SOLO, track, solo[track] ? Colour.YELLOW_FULL : Colour.YELLOW_LOW);
-         setCellLED(TrackModeColumn.ARM, track, arm[track] ? Colour.RED_FULL : Colour.RED_LOW);
-         setCellLED(TrackModeColumn.STOP, track, Colour.OFF);
-         setCellLED(TrackModeColumn.RETURN_TO_ARRANGEMENT, track, Colour.OFF);
-      }
-      else
-      {
-         for(var scene=0; scene<5; scene++)
-         {
-            setCellLED(scene, track, Colour.OFF);
-         }
-      }
+		  if (trackExists[track])
+		  {
+			 setCellLED(TrackModeColumn.STOP, track, Colour.OFF);
+			 setCellLED(TrackModeColumn.SELECT, track, isSelected[track] ?  Colour.GREEN_FLASHING : Colour.GREEN_LOW);
+			 setCellLED(TrackModeColumn.ARM, track, arm[track] ? Colour.RED_FULL : Colour.RED_LOW);
+			 setCellLED(TrackModeColumn.SOLO, track, solo[track] ? Colour.YELLOW_FULL : Colour.YELLOW_LOW);
+			 setCellLED(TrackModeColumn.MUTE, track, mute[track] ? Colour.ORANGE : Colour.AMBER_LOW);
+			 setCellLED(TrackModeColumn.RETURN_TO_ARRANGEMENT, track, Colour.OFF);
+		  }
+		  else
+		  {
+			 for(var scene=0; scene<5; scene++)
+			 {
+				setCellLED(scene, track, Colour.OFF);
+			 }
+		  }
+	 }
+	 else {
+	 		  for(var scene=0; scene<2; scene++)
+		  {
+			 setCellLED(track, scene, Colour.OFF);
+		  }
+
+		  if (trackExists[track])
+		  {
+			 setCellLED(track, TrackModeColumn.STOP+2, Colour.OFF);
+			 setCellLED(track, TrackModeColumn.SELECT+2, isSelected[track] ?  Colour.GREEN_FLASHING : Colour.GREEN_LOW);
+			 setCellLED(track, TrackModeColumn.ARM+2, arm[track] ? Colour.RED_FULL : Colour.RED_LOW);
+			 setCellLED(track, TrackModeColumn.SOLO+2, solo[track] ? Colour.YELLOW_FULL : Colour.YELLOW_LOW);
+			 setCellLED(track, TrackModeColumn.MUTE+2, mute[track] ? Colour.ORANGE : Colour.AMBER_LOW);
+			 setCellLED(track, TrackModeColumn.RETURN_TO_ARRANGEMENT+2, Colour.OFF);
+		  }
+		  else
+		  {
+			 for(var scene=2; scene<7; scene++)
+			 {
+				setCellLED(track, scene, Colour.OFF);
+			 }
+		  }
+	 }
    }
+   // Sets the colour of buttons on the Volume Mode, it is special because of the Vumeter so is split from the rest below
    else if (TEMPMODE == TempMode.VOLUME)
    {
       for(var scene=0; scene<8; scene++)
@@ -387,9 +472,10 @@ gridPage.updateTrackValue = function(track)
                ? Colour.GREEN_LOW
                : Colour.OFF;
 
-         setCellLED(scene, track, c);
+         setCellLED(this.mixerAlignedGrid ? track : scene, this.mixerAlignedGrid ? -Math.abs(scene)+7 : track, c);
       }
    }
+   // Colouring of all other pages such as Pan, Sends and User are drawn here
    else
    {
       var value = 0;
@@ -405,10 +491,12 @@ gridPage.updateTrackValue = function(track)
 
          case TempMode.SEND_A:
             value = sendA[track];
+			oncolor = Colour.YELLOW_FULL;
             break;
 
          case TempMode.SEND_B:
             value = sendB[track];
+			oncolor = Colour.YELLOW_FULL;
             break;
 
          case TempMode.USER1:
@@ -427,10 +515,12 @@ gridPage.updateTrackValue = function(track)
 
       for(var scene=0; scene<8; scene++)
       {
-         setCellLED(scene, track, (scene < drawVal) ? oncolor : offcolor);
+         setCellLED(this.mixerAlignedGrid ? track : scene, this.mixerAlignedGrid ? -Math.abs(scene)+7 : track, (scene < drawVal) ? oncolor : offcolor);
       }
    }
 };
+
+
 
 gridPage.setTempMode = function(mode)
 {
@@ -438,7 +528,7 @@ gridPage.setTempMode = function(mode)
 
    TEMPMODE = mode;
 
-   // Update indications in the app
+   // This updates the indicators (The rainbow displays on dials for controlls (userControls number 3 is missing? from original script)
    for(var p=0; p<8; p++)
    {
       var track = trackBank.getTrack(p);
@@ -446,8 +536,8 @@ gridPage.setTempMode = function(mode)
       track.getPan().setIndication(mode == TempMode.PAN);
       track.getSend(0).setIndication(mode == TempMode.SEND_A);
       track.getSend(1).setIndication(mode == TempMode.SEND_B);
-
       userControls.getControl(p).setIndication(mode == TempMode.USER1);
       userControls.getControl(p + 8).setIndication(mode == TempMode.USER2);
+      userControls.getControl(p + 16).setIndication(mode == TempMode.USER3);
    }
 };
